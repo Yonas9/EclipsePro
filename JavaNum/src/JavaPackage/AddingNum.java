d25b95eb29e0fc4d7963998d444350995920f0f2
Yonas Sh <yonakifle@yahoo.com>
2021-08-26 22:08:44 -0700

    AddingNum

---
+++ JavaNum/src/JavaPackage/AddingNum.java
@@ -0,0 +1,14 @@
+package JavaPackage;
+
+public class AddingNum {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int num1 = 3;
+		int num2 =4;
+		System.out.println(num1 + num2);
+
+	}
+
+}
