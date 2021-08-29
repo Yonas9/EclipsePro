0aa529c8a0ca53d94b130a42155c6d4674b180a4
Yonas Sh <yonakifle@yahoo.com>
2021-08-29 02:24:59 -0700

    update

--- Star/src/Pyramid/Star.java
+++ Star/src/Pyramid/Star.java
@@ -5,7 +5,7 @@ public class Star {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		for(int i=0;i<=5;i++){
+		for(int i=1;i<=5;i++){
 			for(int j=5;j>5-i;j--) {
 				System.out.print(" ");
 			}
