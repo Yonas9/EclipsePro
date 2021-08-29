03ad23677e441584080223bdabd8d0a89fb6352f
Yonas Sh <yonakifle@yahoo.com>
2021-08-29 02:37:45 -0700

    update

--- Star/src/Pyramid/Star.java
+++ Star/src/Pyramid/Star.java
@@ -7,7 +7,7 @@ public class Star {
 		
 		for(int i=1;i<=5;i++){
 			for(int j=5;j>i;j--) {
-				System.out.print("");
+				System.out.print(" ");
 			}
 			for(int k=1;k<=2*i-1;k++) {
 				System.out.print("*");
